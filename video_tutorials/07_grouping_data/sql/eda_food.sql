09830191b42739ccb1672e4b2cb050b4e29f0d61
FellanNokes <felix.l.kjellberg@gmail.com>
2025-11-17 12:05:20 +0100

    grouping data

---
+++ video_tutorials/07_grouping_data/sql/eda_food.sql
@@ -0,0 +1,14 @@
+FROM food;
+
+
+SELECT DISTINCT id FROM food;
+
+-- 201 distinct items
+SELECT COUNT(DISTINCT id) FROM food;
+
+-- 135025 rows
+SELECT COUNT(*) AS number_rows FROM food;
+
+DESC food;
+
+SELECT * FROM food WHERE week_id BETWEEN '2004-04' AND '2004-06';
